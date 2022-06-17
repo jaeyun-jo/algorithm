4375750b00da20434eb0fd779e532a8820c84b81
조재윤 <77223336+jaeyun-jo@users.noreply.github.com>
2022-06-17 22:50:21 +0900

    6월 2주차_실패율 코틀린으로 변경

--- src/실패율.java
+++ src/실패율.java
@@ -1,43 +1,24 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.stream.Collectors
+import java.util.stream.IntStream
 
 /**
  * https://programmers.co.kr/learn/courses/30/lessons/42889
  */
-public class 실패율 {
-    public static void main(String[] args) {
-        int[] stages = { 2, 1, 2, 6, 2, 4, 3, 3 };
-        solution(3, stages);
-    }
+fun main(args: Array<String>) {
+    실패율().solution(8, intArrayOf(1, 2, 3, 4, 5, 6, 7))
+}
 
-    public static int[] solution(int N, int[] stages) {
-        Map<Integer, Long> count = Arrays.stream(stages)
-                                         .boxed()
-                                         .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-        Map<Integer, Double> stageFailRateMap = new HashMap<>();
-        IntStream.range(0, N)
-                 .forEach(i -> {
-                     double tryCount = 0;
-                     for (int j = i + 1; j < stages.length; j++) {
-                         tryCount += count.getOrDefault(j, 0L);
-                     }
-                     if(tryCount == 0) {
-                         stageFailRateMap.put(i + 1, 0.0);
-                     } else {
-                         stageFailRateMap.put(i + 1, count.getOrDefault(i + 1, 0L) / tryCount);
-                     }
-                 });
-
-       return stageFailRateMap.keySet().stream()
-                                       .sorted((o1, o2) -> {
-                                           int first = stageFailRateMap.get(o2).compareTo(stageFailRateMap.get(o1));
-                                           return first == 0 ? o1.compareTo(o2) : first;
-                                       })
-                                       .mapToInt(i -> i)
-                                       .toArray();
+class 실패율 {
+    fun solution(N: Int, stages: IntArray): IntArray {
+        return IntStream.range(1, N + 1).mapToObj { s ->
+            when (val tryCount = stages.count { it >= s }) {
+                0 -> Pair(s, 0.0)
+                else -> Pair(s, stages.count { it == s } / tryCount.toDouble())
+            }
+        }.sorted { o1, o2 ->
+            o2.second.compareTo(o1.second)
+        }.map {
+            it.first
+        }.collect(Collectors.toList()).toIntArray()
     }
 }
